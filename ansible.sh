16027a9d3f4d06fe15fa63e2c2d0bc4badfd4e27
Vairavmoorthy <gmoorthy14@gmail.com>
2023-06-28 22:32:12 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-apt-get update
-apt-get install -y ansible
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 523b153223133db232092d8fcb7699db04b78ea8
+sudo apt-get update
+sudo apt-get install -y ansible
